91880ab08c7dc3d5be5cb899266090aa91fc33dd
2002jan <2002jan@gmail.com>
2022-03-14 13:11:33 +0100

    Added file reading

--- main.c
+++ main.c
@@ -5,12 +5,29 @@ int main(int argc, char** argv)
 {
     if (argc < 3){
         printf("Too little arguments");
-        return 0;
+        return 1;
     }
 
     char *path = argv[1];
+    char location[61];
 
-    printf("size of %s is %lld", path , strlen(path));
+    strcpy(location, argv[2]);
+
+    FILE* fptr;
+
+    fptr = fopen(path, "r");
+
+    if (fptr == NULL){
+        printf("No such ,ini file");
+        return 1;
+    }
+
+    char p[100];
+    while(fscanf(fptr, "%s ", p) == 1){
+        printf("%s\n", p);
+    }
+
+    fclose(fptr);
 
     return 0;
 
