44c94be84348da2641f753650246c81ad99d2e6d
Jan <2002jan@gmail.com>
2022-03-15 17:18:09 +0100

    Added dynamic file read

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv)
 {
@@ -22,10 +23,40 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    char p[100];
-    while(fscanf(fptr, "%s ", p) == 1){
-        printf("%s\n", p);
+    char chunk[10];
+    size_t len = sizeof(chunk);
+    char *line = malloc(len);
+
+    line[0] = "\0";
+
+    while(fgets(chunk, sizeof(chunk), fptr) != NULL){
+        
+        size_t line_used = strlen(line);
+        size_t chunk_used = strlen(chunk);
+
+        if (len - line_used < chunk_used)
+        {
+            len *= 2;
+            if ((line = realloc(line, len)) == NULL)
+            {
+                perror("Unable to reallocate memroy for line buffer");
+                free(line);
+                exit(1);
+            }
+            
+        }
+
+        strncpy(line + line_used, chunk, len - line_used);
+        line_used += chunk_used;
+
+        if (line[line_used - 1] == '\n')
+        {
+            printf("%s which has a length of %d out of %d \n", line, line_used, len);
+            line[0] = '\0';
+        }
     }
+
+    free(line);
 
     fclose(fptr);
 
