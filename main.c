c93744ba14df3675b0aba8485a607619834d0ba8
2002jan <2002jan@gmail.com>
2022-03-23 19:57:47 +0100

    Added expression support

--- main.c
+++ main.c
@@ -17,6 +17,8 @@ struct section
     int keyCount;
 };
 
+void findSectionKey(struct section **sections, int n, char *sectionTarget, char *keyTarget, int *section, int *key);
+
 int main(int argc, char **argv)
 {
     if (argc < 3)
@@ -26,9 +28,6 @@ int main(int argc, char **argv)
     }
 
     char *path = argv[1];
-
-    char *sectionTarget = strtok(argv[2], ".");
-    char *keyTarget = strtok(NULL, ".");
 
     int sectionCount = 0;
     struct section **sections;
@@ -148,26 +147,127 @@ int main(int argc, char **argv)
             line[0] = '\0';
         }
     }
-
     free(line);
 
     fclose(fptr);
 
+    if (strcmp(argv[2], "expression"))
+    {
+        char *sectionTarget = strtok(argv[2], ".");
+        char *keyTarget = strtok(NULL, ".");
+
+        int targetSection = -1, targetKey = -1;
+
+        findSectionKey(sections, sectionCount, sectionTarget, keyTarget, &targetSection, &targetKey);
+
+        printf("Found value %s\nIn section: %s, with key: %s\n", sections[targetSection]->keys[targetKey]->value, sections[targetSection]->name, sections[targetSection]->keys[targetKey]->name);
+    }
+    else
+    {
+        char *exp1 = strtok(argv[3], " ");
+        char *operator= strtok(NULL, " ");
+        char *exp2 = strtok(NULL, " ");
+
+        char *sec1 = strtok(exp1, ".");
+        char *key1 = strtok(NULL, ".");
+
+        char *sec2 = strtok(exp2, ".");
+        char *key2 = strtok(NULL, ".");
+
+        int targetSection = -1, targetKey = -1;
+        findSectionKey(sections, sectionCount, sec1, key1, &targetSection, &targetKey);
+
+        char *val1 = sections[targetSection]->keys[targetKey]->value;
+
+        findSectionKey(sections, sectionCount, sec2, key2, &targetSection, &targetKey);
+
+        char *val2 = sections[targetSection]->keys[targetKey]->value;
+
+        enum valueType type1 = checkValueType(val1), type2 = checkValueType(val2);
+
+        if (type1 != type2)
+        {
+            printf("Diffrent value types!");
+            return 1;
+        }
+
+        if (type1 == Text)
+        {
+            if (operator[0] == '+')
+            {
+                printf("The result of expresstion: %s is\n", argv[3]);
+                printf("%s %s", val1, val2);
+            }
+            else
+            {
+                printf("Wrong expression operator");
+                return 1;
+            }
+        }
+
+        if (type1 == Number)
+        {
+            int num1 = praseInt(val1, strlen(val1)), num2 = praseInt(val2, strlen(val2));
+
+            switch (operator[0])
+            {
+            case '*':
+                num1 *= num2;
+                break;
+            case '+':
+                num1 += num2;
+                break;
+            case '-':
+                num1 -= num2;
+                break;
+            case '/':
+                num1 /= num2;
+                break;
+
+            default:
+                printf("Wrong expression operator");
+                return 1;
+                break;
+            }
+                printf("The result of expresstion: %s is\n", argv[3]);
+                printf("%s %c %s = %d",val1, operator[0], val2, num1);
+        }
+    }
+
     for (int i = 0; i < sectionCount; i++)
+    {
+        for (int j = 0; j < sections[i]->keyCount; j++)
+        {
+            free(sections[i]->keys[j]->name);
+            free(sections[i]->keys[j]->value);
+            free(sections[i]->keys[j]);
+        }
+        free(sections[i]->name);
+        free(sections[i]->keys);
+        free(sections[i]);
+    }
+
+    free(sections);
+
+    return 0;
+}
+
+void findSectionKey(struct section **sections, int n, char *sectionTarget, char *keyTarget, int *section, int *key)
+{
+    for (int i = 0; i < n; i++)
     {
         if (!strcmp(sections[i]->name, sectionTarget))
         {
-            printf("\nSection %s:\n", sections[i]->name);
+            *section = i;
 
             for (int j = 0; j < sections[i]->keyCount; j++)
             {
                 if (!strcmp(sections[i]->keys[j]->name, keyTarget))
                 {
-                    printf("Key: %s, Value: %s\n", sections[i]->keys[j]->name, sections[i]->keys[j]->value);
+                    *key = j;
+                    return;
                 }
             }
         }
     }
-
-    return 0;
 }
